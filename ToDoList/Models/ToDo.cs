eeb045e9f87f98c7d442cd5c4617766a34b2c178
Kaique <Kaique@REVISION-PC>
2024-01-31 09:36:58 -0300

    Adicionar arquivos de projeto.

---
+++ ToDoList/Models/ToDo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToDoList.Models
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+        public string Título { get; set; }
+        public string Descrição { get; set; }
+        public DateTime Vencimento { get; set; }
+        public bool Feito { get; set; }
+        public virtual ApplicationUser User { get; set; }
+    }
+}
